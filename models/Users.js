fbe6cc7d4cbd35356053e73b2fa430d3161d32f4
Tito <titosi099@gmail.com>
2016-10-25 10:58:26 -0300

    Video 13 - Model User e ecriptação de senha com bcrypt

---
+++ models/Users.js
@@ -0,0 +1,43 @@
+import bcrypt from 'bcrypt';
+
+export default (sequelize, DataType) => {
+  const Users = sequelize.define('Users', {
+    id: {
+      type: DataType.INTEGER,
+      primaryKkey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataType.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    password: {
+      type: DataType.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    }
+  },
+  { hooks: {
+    beforeCreate: (user) => {
+      const salt = bcrypt.genSaltSync();
+      user.set('password', bcrypt.hashSync(user.password, salt));
+    }
+  },
+  calssMethods: {
+    isPassword: (encodedPassword, password) => bcrypt.compareSync(password, encodedPassword)
+  }
+});
+  return Users;
+}
